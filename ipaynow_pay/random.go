955e3d84ec65438cb16d8b235ab7f9345142c627
张江南 <zhangjiangnan@ipaynow.cn>
2017-10-26 15:31:53 +0800

    1

---
+++ ipaynow_pay/random.go
@@ -0,0 +1,17 @@
+package ipaynow_pay
+
+import (
+	"math/rand"
+	"time"
+)
+
+func getRandomString(l int) string {
+	str := "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	bytes := []byte(str)
+	result := []byte{}
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := 0; i < l; i++ {
+		result = append(result, bytes[r.Intn(len(bytes))])
+	}
+	return string(result)
+}
